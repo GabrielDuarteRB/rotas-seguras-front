ef6ba49cf900b5ba0f83b8753d90d28fd14fe2d5
Nicollas <nicollaslealsantos@gmail.com>
2025-07-13 18:16:53 -0300

    Adicionados botões no home para Postos de Guarda, Policial e Postos Policiais

--- src/app/(private)/civil/page.tsx
+++ src/app/(private)/civil/page.tsx
@@ -18,7 +18,7 @@ export default function PrivateCivil({
   return (
     <div>
       <h1 className="text-primary text-2xl font-bold">Bem vindo a rotas seguras, {user?.nome}!</h1>
-      <div className="flex mt-16">
+        <div className="flex mt-16 gap-x-8 justify-center">
         <CardOption
           title="Ocorrencias"
           paragraph="Caso tenha acontecido algo com voce, crie uma ocorrencia para podermos ajudar o mais rapido possivel"
@@ -26,10 +26,22 @@ export default function PrivateCivil({
           buttonLink="/civil/ocorrencia"
         />
         <CardOption
+          title="Postos de Guarda"
+          paragraph="Veja os postos de guarda cadastrados."
+          buttonText="Postos"
+          buttonLink="/civil/posto"
+        />
+        <CardOption
           title="Policiais"
           paragraph="Veja uma lista de todos os policiais, ativos ou não ativos."
           buttonText="Policiais"
           buttonLink="/civil/policial"
+        />
+        <CardOption
+          title="Postos Policiais"
+          paragraph="Visualize os postos policiais cadastrados no sistema."
+          buttonText="Postos Policiais"
+          buttonLink="/civil/posto-policial"
         />
       </div>
     </div>
